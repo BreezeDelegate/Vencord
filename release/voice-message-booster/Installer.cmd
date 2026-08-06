20ccf39c130db95922bafc155daff46a52ad6072
Breeze <joeabysseyes+2@gmail.com>
2026-08-06 17:03:43 +0200

    Remove custom VoiceMessageBooster installer

--- release/voice-message-booster/Installer.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-chcp 65001 >nul
-cd /d "%~dp0"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0install.ps1"
-set "EXITCODE=%ERRORLEVEL%"
-if not "%EXITCODE%"=="0" pause
-exit /b %EXITCODE%
